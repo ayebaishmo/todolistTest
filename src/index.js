626e4e1734b058c93b4b3ed8a379adfa00db316b
ayebaishmo <ishmo255super@gmail.com>
2021-12-07 06:45:56 -0800

    Set up html and js file

--- src/index.js
+++ src/index.js
@@ -1,2 +1,29 @@
 import _ from 'lodash';
 import './style.css';
+
+const tasks = [
+    {
+        description: 'Wash dishes',
+        completed: false,
+        index: 1
+    },
+    {
+        description: 'Go to church',
+        completed: false,
+        index: 2
+    }
+];
+
+const listTasks = document.querySelector('.listTasks');
+
+for (let i = 0; i < tasks.length; i += 1) {
+    const listItem = document.createElement('li');
+    const input = document.createElement('input');
+    input.setAttribute('type', 'checkbox');
+    listItem.appendChild(input);
+    const label = document.createElement('label');
+    listItem.appendChild(label);
+    label.textContent = tasks[i].description;
+    listTasks.appendChild(listItem);
+}
+
