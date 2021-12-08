bda4783e2663db1520c695aea3e929c4646c1389
ayebaishmo <ishmo255super@gmail.com>
2021-12-08 12:02:47 -0800

    Change task to complted = true

--- src/index.js
+++ src/index.js
@@ -26,11 +26,15 @@ const displayTasks = () => {
     const input = document.createElement('input');
     input.setAttribute('type', 'checkbox');
     input.setAttribute('class', 'checkboxed');
+    input.setAttribute('id', '${tasks.index}');
     listItem.appendChild(input);
     const label = document.createElement('label');
+    const label2 = document.createElement('label2');
     label.setAttribute('class', 'label');
     listItem.appendChild(label);
+    listItem.appendChild(label2);
     label.textContent = tasks[i].description;
+    label2.textContent = tasks[i].completed;
     listTasks.appendChild(listItem);
   }
 };
@@ -42,15 +46,20 @@ const checkboxed = document.getElementsByClassName('checkboxed');
 
 for (let j = 0; j < checkboxed.length; j++) {
   
-checkboxed[j].addEventListener('change', () => {
-  if(checkboxed[j].checked) {
-    checkboxed[j].nextElementSibling.style.textDecoration = 'line-through';
-  } else {
-        checkboxed[j].nextElementSibling.style.textDecoration = 'none';
-  }
-});
-
+  checkboxed[j].addEventListener('change', () => {
+    if(checkboxed[j].checked) {
+      checkboxed[j].nextElementSibling.style.textDecoration = 'line-through';
+    } else {
+      checkboxed[j].nextElementSibling.style.textDecoration = 'none';
+    }
+  });
 }
 
-
+const itemElement = document.getElementById(tasks.index);
+const lab = itemElement.parentElement.querySelector('label');
+if (itemElement.checked) {
+  tasks.completed = true;
+} else {
+  tasks.completed = false;
+}
 // window.onload = displayTasks;
