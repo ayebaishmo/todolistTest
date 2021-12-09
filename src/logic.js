26b05b71b90209bf7418487445a22422d599fbbe
ayebaishmo <ishmo255super@gmail.com>
2021-12-09 13:40:30 -0800

    Import function to toggle the task

--- src/logic.js
+++ src/logic.js
@@ -18,3 +18,16 @@ export const itemIncomplete = (item, items) => {
   }
   item.index = 0;
 };
+
+export const toggleComplete = (item, items) => {
+  const itemElement = document.getElementById(item.index);
+  const label = itemElement.parentElement.querySelector('label');
+  if (itemElement.checked) {
+    item.complete = true;
+    label.classList = (item.complete);
+  } else {
+    item.complete = false;
+    label.classList = (item.complete);
+  }
+  ls.saveListData(items, ls.saveDataLocation);
+};
--- src/status.js
+++ src/status.js
@@ -1,76 +1,51 @@
-const listTasks = document.querySelector('.listTasks');
-class Task {
-  constructor(description, completed = false, index = 0) {
-    this.description = description;
-    this.completed = completed;
-    this.index = index;
-  }
+import { toggleComplete } from './logic.js';
 
-  static tasks = [];
+export function defaultList(item, i, items) {
+  const frag = document.createDocumentFragment();
+  const list = document.createElement('li');
+  const div = document.createElement('div');
+  const check = document.createElement('input');
+  const label = document.createElement('label');
+  const spanDiv = document.createElement('div');
+  const span = document.createElement('span');
+  const altSpan = document.createElement('span');
 
-  static getDataFromLocalStorage() {
-    if (!window.localStorage.getItem('tasksData')) return null;
-      const data = JSON.parse(window.localStorage.getItem('tasksData'));
-      data.myTasks = data.mmyBooks.map((b) => {
-          const task = new Task(b.description, b.completed, b.index);
-          return task;
-      });
-      Task.tasks = data.myTasks;
-      return data;
-  }
+  div.appendChild(check);
+  div.appendChild(label);
+  list.appendChild(div);
+  list.appendChild(spanDiv);
+  spanDiv.appendChild(span);
+  spanDiv.appendChild(altSpan);
+  frag.appendChild(list);
 
-  static setDataInLocalStorage(data) {
-    window.localStorage.setItem('tasksData', JSON.stringify(data));
-  }
+  div.classList.add('todo', 'input', `${item.index}`);
 
-  static displayTasks() {
-    const tasks = Task.getDataFromLocalStorage();
-    Task.tasks.forEach((task) => {
-      const listItem = document.createElement('li');
-      const input = document.createElement('input');
-      input.setAttribute('type', 'checkbox');
-      input.setAttribute('class', 'checkboxed');
-      input.setAttribute('id', `${task.index}`);
-      listItem.appendChild(input);
-      const label = document.createElement('label');
-      label.setAttribute('class', 'label');
-      listItem.appendChild(label);
-      label.textContent = task.description;
-      listTasks.appendChild(listItem);
-    });
-  }
-  addTask() {
-      this.index = tasks.taskCount + 1;
-      tasks.myTasks.push(this);
-      tasks.taskCount +=1;
-      Task.setDataInLocalStorage(tasks);
-      Task.displayTasks();
-      description.value = '';
-      completed.value = false;
-  }
+  check.type = 'checkbox';
+  check.name = item.index;
+  check.id = item.index;
+  check.checked = item.complete;
+
+  label.classList = (item.complete);
+  label.htmlFor = item.index;
+  label.textContent = item.description;
+
+  list.id = `${i}li`;
+
+  span.innerHTML = '&#8942;';
+  span.classList.add('icon', 'options-icon');
 }
 
-// const status = () => {
-//   const checkboxed = document.getElementsByClassName('checkboxed');
-//   for (let j = 0; j < checkboxed.length; j += 1) {
-//     checkboxed[j].addEventListener('change', () => {
-//       if (checkboxed[j].checked) {
-//         checkboxed[j].nextElementSibling.style.textDecoration = 'line-through';
-//         tasks[j].completed = true;
-//         localStorage.setItem('tasks', JSON.stringify(tasks));
-//       } else {
-//         checkboxed[j].nextElementSibling.style.textDecoration = 'none';
-//         tasks[j].completed = false;
-//         localStorage.setItem('tasks', JSON.stringify(tasks));
-//       }
-//     });
-//   }
-// };
-
-// const add = () => {
-//   const btn = document.getElementById('arrow');
-//   btn.addEventListener('click', () => {
-
-//   })
-// }
-// export { status, displayTasks, tasks };
+export const documentToDo = (list) => {
+  if (list) {
+    const wrapper = document.querySelector('.items');
+    wrapper.innerHTML = '';
+    for (let i = 0; i < list.length; i += 1) {
+      const todoItem = list.filter((item) => item.index === i)[0];
+      const completeList = defaultList(todoItem, i, list);
+      completeList.check.addEventListener('click', () => {
+        toggleComplete(todoItem, list);
+      });
+      wrapper.appendChild(completeList.frag);
+    }
+  }
+};
