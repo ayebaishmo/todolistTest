b278540153c4c278cd3d11029f8e413e92de610c
Dean <deikdesign@gmail.com>
2021-12-16 20:47:04 +0200

    RemoveAll test added

--- src/add-remove.test.js
+++ src/add-remove.test.js
@@ -124,7 +124,7 @@ describe('Remove...', () => {
       complete: true,
       index: 3,
     }];
-    const newArr = addRemove.removeAllCompleted(itemsFull);
+    const newArr = addRemove.removeAll(itemsFull);
     documentToDo(newArr);
     expect(document.body.querySelectorAll('li').length).toBe(2);
   });
