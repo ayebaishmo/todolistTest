23260f7d248af7af577a389d22e269176176038c
ayebaishmo <ishmo255super@gmail.com>
2021-12-16 09:17:10 -0800

    Add test for testing Add list itm to the ul

---
+++ src/add-remove.test.js
@@ -0,0 +1,77 @@
+import * as addRemove from './add-remove.js';
+import documentToDo from './status.js';
+
+jest.mock('./storage');
+jest.mock('./status');
+
+const itemsFull = [{
+  description: 'gohome',
+  complete: false,
+  index: 0,
+},
+{
+  description: 'comeback',
+  complete: true,
+  index: 1,
+}];
+
+describe('Add...', () => {
+  test('Abc to empty items', () => {
+    const input = { value: 'Abc' };
+    const itemsEmpty = [];
+    addRemove.addItem(input, itemsEmpty);
+    expect(itemsEmpty.length).toBe(1);
+  });
+
+  test('Abc to full items', () => {
+    const input = { value: 'Abc' };
+    const itemsFull = [{
+      description: 'xyzzy',
+      complete: false,
+      index: 0,
+    },
+    {
+      description: 'abcde',
+      complete: true,
+      index: 1,
+    }];
+    addRemove.addItem(input, itemsFull);
+    expect(itemsFull.length).toBe(3);
+  });
+
+  test('Abc to Empty DOM', () => {
+    document.body.innerHTML = `
+    <ul class="todo items">
+    </ul>
+    `;
+    const input = { value: 'Abc' };
+    const itemsEmpty = [];
+    addRemove.addItem(input, itemsEmpty);
+    documentToDo(itemsEmpty);
+    expect(document.body.querySelectorAll('li').length).toBe(1);
+  });
+
+  test('Abc to NonEmpty DOM', () => {
+    document.body.innerHTML = `
+    <ul class="todo items">
+    </ul>
+    `;
+    const input = { value: 'Abc' };
+    const itemsFull = [{
+      description: 'xyzzy',
+      complete: false,
+      index: 0,
+    },
+    {
+      description: 'abcde',
+      complete: true,
+      index: 1,
+    }];
+    addRemove.addItem(input, itemsFull);
+    documentToDo(itemsFull);
+    expect(document.body.querySelectorAll('li').length).toBe(3);
+  });
+});
+
+
+
